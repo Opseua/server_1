968f6e8a6feaef02644d41c324e40aac898c4fe7
Opseua <opseua@gmail.com>
2022-11-20 22:31:08 -0300

    .

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-server.use(router)
-server.listen(3000, () => {
-  console.log('JSON Server is running')
-})
+const port = process.env.PORT || 8080;
+
+server.use(middlewares);
+server.use(jsonServer.rewriter({
+  "/todos_os_funcionarios": "/funcionarios",
+  "/adicionar_funcionario": "/funcionarios",
+  "/editar_funcionario/:id": "/funcionarios/:id",
+  "/deletar_funcionario/:id": "/funcionarios/:id",
+  "/teste": "/funcionarios"
+}))
+server.use(router);
+server.listen(port, () => { console.log(`JSON Server está rodando na porta ${port}`) })
