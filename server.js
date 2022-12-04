8f634d35604b596645a8d23b1f780402bc8708b3
Orlando <opseua@gmail.com>
2022-12-04 19:02:46 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,31 +8,31 @@ const port = process.env.PORT || 8080;
 server.use(middlewares);
 server.use(jsonServer.rewriter({
   "/corretores/listar": "/corretores",
-  "/corretor/criar": "/corretores",
+  "/corretor/adicionar": "/corretores",
   "/corretor/detalhar/:id": "/corretores/:id",
   "/corretor/editar/:id": "/corretores/:id",
   "/corretor/deletar/:id": "/corretores/:id",
 
   "/formasdepagamento/listar": "/formasdepagamento",
-  "/formadepagamento/criar": "/formasdepagamento",
+  "/formadepagamento/adicionar": "/formasdepagamento",
   "/formadepagamento/detalhar/:id": "/formasdepagamento/:id",
   "/formadepagamento/editar/:id": "/formasdepagamento/:id",
   "/formadepagamento/deletar/:id": "/formasdepagamento/:id",
 
   "/imoveiscomerciais/listar": "/imoveiscomerciais",
-  "/imovelcomercial/criar": "/imoveiscomerciais",
+  "/imovelcomercial/adicionar": "/imoveiscomerciais",
   "/imovelcomercial/detalhar/:id": "/imoveiscomerciais/:id",
   "/imovelcomercial/editar/:id": "/imoveiscomerciais/:id",
   "/imovelcomercial/deletar/:id": "/imoveiscomerciais/:id",
   
   "/imoveisresidenciais/listar": "/imoveisresidenciais",
-  "/imovelresidencial/criar": "/imoveisresidenciais",
+  "/imovelresidencial/adicionar": "/imoveisresidenciais",
   "/imovelresidencial/detalhar/:id": "/imoveisresidenciais/:id",
   "/imovelresidencial/editar/:id": "/imoveisresidenciais/:id",
   "/imovelresidencial/deletar/:id": "/imoveisresidenciais/:id",
 
   "/inquilinos/listar": "/inquilinos",
-  "/inquilino/criar": "/inquilinos",
+  "/inquilino/adicionar": "/inquilinos",
   "/inquilino/detalhar/:id": "/inquilinos/:id",
   "/inquilino/editar/:id": "/inquilinos/:id",
   "/inquilino/deletar/:id": "/inquilinos/:id",
